e96a19a9b7fde6c7ecf5872195c957b62ee544b6
artivashnavi <artivashnavirathaur@gmail.com>
2023-01-21 14:01:54 +0530

    form-1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
+import {Component} from "react";
+import FormComp1 from "./FormComponent/FormComp1";
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
+class App extends Component{
+  render(){
+      return (
+        <div className="App">
+          <FormComp1 />
+        </div>
+      );
+  }
+}                                                                                                                                                                               
 export default App;
